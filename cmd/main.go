e1ece0c2605483b7a33315b72c228aae6094fac7
Emily <emilymarston@outlook.com.au>
2024-05-14 13:44:31 +1000

    update docker files

--- cmd/main.go
+++ cmd/main.go
@@ -12,14 +12,13 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/bh1995/AstroStreakNet/database/pkg/routes"
 	"github.com/gorilla/mux"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 func main() {
 	r := mux.NewRouter()
-	routes.RegisterUserRoutes(r)
+	//routes.RegisterUserRoutes(r)
 	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe("localhost:3306", r))
 }
